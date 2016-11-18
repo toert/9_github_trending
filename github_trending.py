53e79f0ed17f48689e75dcf95f37a1bbe4caaef6
Kondakov Valeriy <pw177@mail.ru>
2016-11-18 15:36:10 +0300

    Update github_trending.py

--- github_trending.py
+++ github_trending.py
@@ -1,41 +1,107 @@
-import requests
-import datetime
-TOP = 20
-
-def get_trending_repositories(top_size):
-    week_ago_date = str(datetime.date.today() - \
-                        datetime.timedelta(days=7))
-    request_to_github = requests.get\
-    ('https://api.github.com/search/repositories?q=created:>{}'.\
-     format(week_ago_date))
-    response_list = request_to_github.json()['items'][:top_size]
-    return response_list
+from os.path import basename, getsize, splitext, join
+import os
+from collections import Counter
+from collections import defaultdict
+import re
 
 
-def get_list_open_issues(repo_owner, repo_name):
-    request_to_github = requests.get('https://api.github.com/repos/{}/{}/issues'\
-                                     .format(repo_owner, repo_name))
-    response_list = request_to_github.json()
-    return list(filter(lambda response: response['state'] == \
-                                        'open',response_list))
+def are_files_duplicates(file_path1, file_path2):
+    name1 = splitext(basename(file_path1))[0]
+    name2 = splitext(basename(file_path2))[0]
+    return bool(getsize(file_path1) == getsize(file_path2) and (name1 == name2))
 
 
-def print_open_issues_of_repo(reposit_json):
-    repo_owner = reposit_json['owner']['login']
-    repo_name = reposit_json['name']
-    stars_count = reposit_json['stargazers_count']
-    list_of_open_issues = get_list_open_issues(repo_owner,repo_name)
-    print('{name}\'s repository {repo} has {stars_count}\
- stars and {issues_count} open issues.'.format(name=repo_owner,\
-    repo=repo_name,stars_count=stars_count,issues_count=len(list_of_open_issues)))
-    if len(list_of_open_issues) > 0:
-        print('Links to issues:')
-    for issue in list_of_open_issues:
-        print(issue['url'])
-    print('')
+def are_files_copies(file_path1, file_path2):
+    name1 = splitext(basename(file_path1))[0]
+    name2 = splitext(basename(file_path2))[0]
+    pattern1 = '{} \(\d\)'.format(name1)
+    pattern2 = '{} \(\d\)'.format(name2)
+    return bool(getsize(file_path1) == getsize(file_path2) and \
+                ((re.match(pattern1, name2)) or (re.match(pattern2, name1))))
+
+
+def get_all_files(start_filepath):
+    all_files = []
+    for root, dirs, files in os.walk(start_filepath, topdown=False):
+        for current_file in files:
+            current_path_of_file = join(str(root), str(current_file))
+            all_files.append(current_path_of_file)
+    return all_files
+
+
+def get_dict_with_sizes(filepath):
+    dict = {}
+    for path in filepath:
+        dict[path] = getsize(path)
+    return dict
+
+
+def get_dict_of_copies(paths): #get copies like 17.txt, 17 (1).txt, 17 (2).txt, etc
+    struct_files = defaultdict(list)
+    only_repeating = defaultdict(list)
+    sizes = get_dict_with_sizes(paths)
+    were_saved = []
+    number_repeat = -1
+    for path_number, path in enumerate(paths[:-1]): #because the last will be compared with all other files
+        if path not in were_saved:
+            number_repeat = number_repeat + 1
+            struct_files[number_repeat].append(path)
+            does_it_have_a_couple = False
+            for other_path_number, other_path in enumerate\
+(list(filter(lambda filepath: sizes[filepath] == sizes[path] , paths[path_number + 1:]))):
+                if are_files_copies(path, other_path):
+                    does_it_have_a_couple = True
+                    struct_files[number_repeat].append(other_path)
+                    were_saved.append(other_path)
+            if does_it_have_a_couple:
+                were_saved.append(path)
+    for key in struct_files:
+        if len(struct_files[key]) > 1:
+            only_repeating[key] = struct_files[key]
+    return only_repeating
+
+
+def get_dict_of_duplicates(paths):
+    struct_files = defaultdict(list)
+    only_repeating = defaultdict(list)
+    sizes = get_dict_with_sizes(paths)
+    were_saved = []
+    number_repeat = -1
+    for path_number, path in enumerate(paths[:-1]): #because the last will be compared with all other files
+        if path not in were_saved:
+            number_repeat = number_repeat + 1
+            struct_files[number_repeat].append(path)
+            does_it_have_a_couple = False
+            for other_path_number, other_path in enumerate\
+(list(filter(lambda filepath: sizes[filepath] == sizes[path] , paths[path_number + 1:]))):
+                if are_files_duplicates(path, other_path):
+                    does_it_have_a_couple = True
+                    struct_files[number_repeat].append(other_path)
+                    were_saved.append(other_path)
+            if does_it_have_a_couple:
+                were_saved.append(path)
+    for key in struct_files:
+        if len(struct_files[key]) > 1:
+            only_repeating[key] = struct_files[key]
+    return only_repeating
+
+
+def get_list_repeating_files(filepath_folder):
+    filepaths_of_all_files = get_all_files(filepath_folder)
+    all_names = []
+    for path in filepaths_of_all_files:
+        all_names.append(basename(path))
+    list_of_counts = Counter(all_names)
+    list_of_counts.most_common()
+    return list(filter(lambda filepath: list_of_counts[basename(filepath)] > 1\
+                       , filepaths_of_all_files))
 
 
 if __name__ == '__main__':
-    top_reps = get_trending_repositories(TOP)
-    for repo in top_reps:
-        print_open_issues_of_repo(repo)
+    filepath_folder = input('Enter filepath to dir: ')
+    filepaths = get_all_files(filepath_folder)
+    repeating_files = get_dict_of_duplicates(filepaths)
+    for key in repeating_files:
+        print('File {} is saved in: '.format(basename(repeating_files[key][0])))
+        for path in repeating_files[key]:
+            print(path)
