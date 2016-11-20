c569596458585dd3b44d03f18a7655a2bb816331
Kondakov Valeriy <pw177@mail.ru>
2016-11-20 12:55:12 +0300

    road to pure functions

--- github_trending.py
+++ github_trending.py
@@ -2,7 +2,8 @@ import requests
 import datetime
 TOP = 20
 
-def get_trending_repositories(top_size):
+
+def fetch_trending_repositories(top_size):
     week_ago_date = str(datetime.date.today() - \
                         datetime.timedelta(days=7))
     request_to_github = requests.get\
@@ -12,30 +13,30 @@ def get_trending_repositories(top_size):
     return response_list
 
 
-def get_list_open_issues(repo_owner, repo_name):
+def fetch_list_open_issues(repo_owner, repo_name):
     request_to_github = requests.get('https://api.github.com/repos/{}/{}/issues'\
                                      .format(repo_owner, repo_name))
     response_list = request_to_github.json()
     return list(filter(lambda response: response['state'] == \
-                                        'open',response_list))
+                                        'open', response_list))
 
 
-def print_open_issues_of_repo(reposit_json):
-    repo_owner = reposit_json['owner']['login']
-    repo_name = reposit_json['name']
-    stars_count = reposit_json['stargazers_count']
-    list_of_open_issues = get_list_open_issues(repo_owner,repo_name)
+def print_open_issues_of_repo(repo_owner, repo_name, stars_count, list_of_issues):
     print('{name}\'s repository {repo} has {stars_count}\
  stars and {issues_count} open issues.'.format(name=repo_owner,\
-    repo=repo_name,stars_count=stars_count,issues_count=len(list_of_open_issues)))
-    if len(list_of_open_issues) > 0:
-        print('Links to issues:')
-    for issue in list_of_open_issues:
+    repo=repo_name,stars_count=stars_count,issues_count=len(list_of_issues)))
+    print('Links to issues:')
+    for issue in list_of_issues:
         print(issue['url'])
     print('')
 
 
 if __name__ == '__main__':
-    top_reps = get_trending_repositories(TOP)
+    top_reps = fetch_trending_repositories(TOP)
     for repo in top_reps:
-        print_open_issues_of_repo(repo)
+        repo_owner = repo['owner']['login']
+        repo_name = repo['name']
+        stars_count = repo['stargazers_count']
+        list_of_open_issues = fetch_list_open_issues(repo_owner, repo_name)
+        if len(list_of_open_issues) > 0:
+            print_open_issues_of_repo(repo_owner, repo_name, stars_count, list_of_open_issues)
